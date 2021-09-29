b0905a0c904c677209adbe9eb387eeeb76d88290
raquel <raquelanaroman@gmail.com>
2021-09-29 16:02:42 -0400

    create sidebar component

---
+++ src/private/Sidebar.js
@@ -0,0 +1,237 @@
+import {
+    Avatar,
+    Toolbar,
+    Divider,
+    IconButton,
+    Typography,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Tooltip, Menu, MenuItem
+} from "@mui/material";
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import GroupsIcon from '@mui/icons-material/Groups';
+import EventIcon from '@mui/icons-material/Event';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import { styled, useTheme } from '@mui/material/styles';
+import { useState } from "react"
+import { Link } from "react-router-dom";
+
+
+
+const drawerWidth = 200;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(9)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  }));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+
+export default function Sidebar() {
+    const [open, setOpen] = useState(false);
+    const [anchor, setAnchor] = useState(null);
+
+    const user = {firstName: 'Raquel', teacher: true}
+
+    const theme = useTheme();
+
+    const handleClick = (e) => {
+        setAnchor(e.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchor(null);
+    };
+
+    const handleOpenDrawer = () => {
+        setOpen(!open);
+    };
+
+    const handleSignOut = () =>{
+        console.log('I want to sign out')
+    }
+
+    return (
+        <>
+            <AppBar position="fixed" open={open}  >
+                <Toolbar sx={{ justifyContent: 'space-between', overflowX: 'auto' }}>
+                    <IconButton
+                        aria-label="open drawer"
+                        onClick={handleOpenDrawer}
+                        edge="start"
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="div">
+                        Maestro
+                    </Typography>
+                    <Tooltip title="Account Settings">
+                        <IconButton onClick={handleClick} size='small' sx={{ ml: 2 }}>
+                            <Avatar sx={{ width: 32, height: 32 }}>{user.firstName.charAt(0)}</Avatar>
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        anchorEl={anchor}
+                        open={Boolean(anchor)}
+                        onClose={handleClose}
+                        onClick={handleClose}
+                        PaperProps={{
+                            elevation: 0,
+                            sx: {
+                                overflow: 'visible',
+                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                mt: 1.5,
+                                '& .MuiAvatar-root': {
+                                    width: 32,
+                                    height: 32,
+                                    ml: -0.5,
+                                    mr: 1,
+                                },
+                                '&:before': {
+                                    content: '""',
+                                    display: 'block',
+                                    position: 'absolute',
+                                    top: 0,
+                                    right: 25,
+                                    width: 10,
+                                    height: 10,
+                                    bgcolor: 'background.paper',
+                                    transform: 'translateY(-50%) rotate(45deg)',
+                                    zIndex: 0,
+                                },
+                            },
+                        }}
+                        transformOrigin={{ horizontal: 'center', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem component={Link} to='/profile'>
+                            <Avatar sx={{ width: 16, height: 16 }} /> Profile
+                        </MenuItem>
+                        <Divider />
+                        <MenuItem component={Link} to='/settings'>
+                            <ListItemIcon>
+                                <Settings fontSize="small" />
+                            </ListItemIcon>
+                            Settings
+                        </MenuItem>
+                        <MenuItem onClick={handleSignOut}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                            Logout
+                        </MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleOpenDrawer}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    <ListItemButton component={Link} to='/dashboard'>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText>Dashboard</ListItemText>
+                    </ListItemButton>
+                    {user.teacher && <ListItemButton component={Link} to='/students'>
+                        <ListItemIcon>
+                            <GroupsIcon />
+                        </ListItemIcon>
+                        <ListItemText>Students</ListItemText>
+                    </ListItemButton>}
+                    <ListItemButton component={Link} to='/calendar'>
+                        <ListItemIcon>
+                            <EventIcon />
+                        </ListItemIcon>
+                        <ListItemText>Calendar</ListItemText>
+                    </ListItemButton>
+                    <ListItemButton component={Link} to='/billing'>
+                        <ListItemIcon>
+                            <MonetizationOnOutlinedIcon />
+                        </ListItemIcon>
+                        <ListItemText>Billing</ListItemText>
+                    </ListItemButton>
+                </List>
+            </Drawer>
+        </>
+    )
+}

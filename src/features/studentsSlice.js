9829bc0323477ac3e73ddefb8a4af960b88af408
raquel <raquelanaroman@gmail.com>
2021-09-29 16:03:20 -0400

    create studentsslice

---
+++ src/features/studentsSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../api";
+
+const initialState = {
+    students: []
+};
+
+export const slice = createSlice({
+    name: "students",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(api.endpoints.students.matchFulfilled, (state, action) => {
+                console.log('fulfilled-login', action);
+            })
+            .addMatcher(api.endpoints.logout.matchFulfilled, (state) => {
+                console.log('fulfilled-logout', state);
+                return initialState;
+            })
+            
+    }
+})
+
+
+export default slice.reducer;

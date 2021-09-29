358e3425eaa7b87224f918af25767387955916ef
raquel <raquelanaroman@gmail.com>
2021-09-29 16:03:10 -0400

    create userslice

---
+++ src/features/userSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { api } from "../api";
+
+const initialState = {
+    email: "",
+    firstName: "",
+    lastName: "",
+    phone: "",
+    role: "",
+    address: "",
+    id:""
+};
+
+export const slice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder
+            .addMatcher(api.endpoints.login.matchPending, (state, action) => {
+                console.log('pending', action);
+            })
+            .addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
+                console.log('fulfilled-login', action);
+                const { id } = action.payload.data;
+                const { firstName, lastName, email, address, phone, role } = action.payload.data.attributes;
+                Object.assign(state, { id, firstName, lastName, email, address, phone, role })
+            })
+            .addMatcher(api.endpoints.login.matchRejected, (state, action) => {
+                console.log('rejected', action);
+            })
+            .addMatcher(api.endpoints.logout.matchFulfilled, (state) => {
+                console.log('fulfilled-logout', state);
+                return initialState;
+            })
+            .addMatcher(api.endpoints.isLoggedIn.matchFulfilled, (state, action) => {
+                console.log('fulfilled-session recovered', action);
+                const { id } = action.payload.data;
+                const { firstName, lastName, email, address, phone, role } = action.payload.data.attributes;
+                Object.assign(state, { id, firstName, lastName, email, address, phone, role })
+            })
+    }
+})
+
+
+export default slice.reducer;

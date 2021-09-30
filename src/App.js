cc7fa3555d1f362d50ee443d56264851d7c93f7d
raquel <raquelanaroman@gmail.com>
2021-09-30 14:38:10 -0400

    change fetch to async for rtk mutation

--- src/App.js
+++ src/App.js
@@ -1,82 +1,43 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
+import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux'
 
+import {useIsLoggedInMutation} from './features/api'
 import Private from './private/Private';
 import Public from './public/Public';
 import './App.css';
-const data = [
-  {
-    id: 1,
-    allDay: false,
-    start: '',
-    end: '',
-    daysOfWeek: [1],
-    startTime: '12:30',
-    endTime: '13:30',
-    startRecur: '2021-09-27',
-    endRecur: '',
-    title: 'Lisa Lesson',
-    type: "Lesson"
-  },
-  {
-    id: 2,
-    allDay: false,
-    start: '',
-    end: '',
-    daysOfWeek: [1],
-    startTime: '13:30',
-    endTime: '14:15',
-    startRecur: '2021-09-27',
-    endRecur: '',
-    title: 'Hannah Lesson',
-    type: "Lesson"
-  },
-  {
-    id: 3,
-    allDay: false,
-    start: '',
-    end: '',
-    daysOfWeek: [1],
-    startTime: '14:30',
-    endTime: '15:30',
-    startRecur: '2021-09-27',
-    endRecur: '',
-    title: 'Dennis Lesson',
-    type: "Lesson"
-  },
-  {
-    id: 4,
-    allDay: false,
-    start: '',
-    end: '',
-    daysOfWeek: [1],
-    startTime: '15:45',
-    endTime: '16:30',
-    startRecur: '2021-09-27',
-    endRecur: '',
-    title: 'Audrianna Lesson',
-    type: "Lesson"
-  },
-]
-
-function handleEventClick(clickInfo){
-  console.log(clickInfo.event)
-}
 
 
 function App() {
+  const [isLoggedIn] = useIsLoggedInMutation();
+  const history = useHistory();
+
+  const checkForToken = useCallback(async () => {
+    try {
+      await isLoggedIn().unwrap()
+      history.push('/dashboard')
+    } catch (error) {
+      console.error()
+      history.push('/home')
+    }
+  }, [isLoggedIn, history]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/me', {
-      headers:{
-        "Content-Type": 'application/json',
-        Authorization: localStorage.getItem("token"),
-      }
-    })
-    .then(r=>r.json())
-    .then(data=>console.log(data))
-  }, [])
+    checkForToken()
+  }, [checkForToken]);
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/me', {
+  //     headers:{
+  //       "Content-Type": 'application/json',
+  //       Authorization: localStorage.getItem("token"),
+  //     }
+  //   })
+  //   .then(r=>r.json())
+  //   .then(data=>console.log(data))
+  // }, [])
 
   return (
     <div className="App">

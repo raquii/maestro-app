882b18b2064df5c4066e7803c335347beebf8da8
raquel <raquelanaroman@gmail.com>
2021-10-01 15:41:00 -0400

    add new pages to Private app

--- src/private/Private.js
+++ src/private/Private.js
@@ -1,32 +1,51 @@
 import { Route, Switch } from "react-router";
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 
 import Sidebar from "./Sidebar";
+import Dashboard from "./Dashboard"
+import Students from "./Students";
+import Calendar from "./Calendar"
+import Settings from "./Settings";
+import Profile from "./Profile";
+import Billing from "./Billing";
 
 export default function Private() {
     //I want a header at the top, that renders the users name in the right corner
     //a sidebar navigation menu with link components for routes
-    
+
     return (
         <Box sx={{ display: 'flex' }}>
             <Sidebar />
-            <Switch>
-                <Route path='/dashboard'>
-                    <h1>Home</h1>
-                </Route>
-                <Route path='/students'>
-                    <h1>Students</h1>
-                </Route>
-                <Route path='/calendar'>
-                    <h1>Calendar</h1>
-                </Route>
-                <Route path='/billing'>
-                    <h1>Biling</h1>
-                </Route>
-                <Route path='/settings'>
-                    <h1>Settings</h1>
-                </Route>
-            </Switch>
+            <Box 
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    height: '100vh',
+                    mt: 8
+                }} 
+            >
+                {/* <Toolbar /> */}
+                <Switch>
+                    <Route path='/dashboard'>
+                        <Dashboard />
+                    </Route>
+                    <Route path='/students'>
+                        <Students />
+                    </Route>
+                    <Route path='/calendar'>
+                        <Calendar />
+                    </Route>
+                    <Route path='/billing'>
+                        <Billing />
+                    </Route>
+                    <Route path='/settings'>
+                        <Settings />
+                    </Route>
+                    <Route path='/profile'>
+                        <Profile />
+                    </Route>
+                </Switch>
+            </Box>
         </Box>
     )
 }

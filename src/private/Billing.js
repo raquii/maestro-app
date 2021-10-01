7f8fd02eb174a818daf89ef66bdb4b448557c46e
raquel <raquelanaroman@gmail.com>
2021-10-01 15:41:26 -0400

    init billing page

---
+++ src/private/Billing.js
@@ -0,0 +1,16 @@
+import { Container } from "@mui/material"
+import MoneyIcon from '@mui/icons-material/MonetizationOnOutlined';
+
+import PageHeader from "./components/PageHeader"
+
+export default function Billing() {
+    return (
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <PageHeader
+                icon={<MoneyIcon fontSize="large" sx={{ mr: 1 }} />}
+                page="Billing"
+            />
+
+        </Container>
+    )
+}

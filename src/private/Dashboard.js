8aea8cce8ae2940d9ddbdc3cf0ec186097f0a7bf
raquel <raquelanaroman@gmail.com>
2021-10-01 15:41:19 -0400

    add grid cards to dashboard component

--- src/private/Dashboard.js
+++ src/private/Dashboard.js
@@ -1,7 +1,46 @@
+import { Container, Grid, Paper } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+
+import PageHeader from "./components/PageHeader";
+
 export default function Dashboard() {
     return (
-        <div>
-            
-        </div>
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <PageHeader
+                icon={<HomeIcon fontSize="large" sx={{ mr: 1 }} />}
+                page="Dashboard"
+            />
+            <Grid container spacing={3}>
+                <Grid item xs={12} md={8} lg={9}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: 240,
+                        }}
+                    >
+
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} md={4} lg={3}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: 240,
+                        }}
+                    >
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+
+                    </Paper>
+                </Grid>
+
+            </Grid>
+        </Container>
     )
 }

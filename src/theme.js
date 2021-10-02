0ee700dc65400e1421fa1eb1fce5056bbb66d041
raquel <raquelanaroman@gmail.com>
2021-10-01 23:59:35 -0400

    create theme for app

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,38 @@
 import { createTheme } from "@mui/material";
 
-export const theme = createTheme();
+export const theme = createTheme({ 
+    palette: {
+    type: 'light',
+    primary: {
+      main: '#1e879a',
+    },
+    secondary: {
+      main: '#e85d5d',
+    },
+    background: {
+      default: '#e0f7fa',
+    },
+    warning: {
+      main: '#f57c00',
+    },
+    error: {
+      main: '#ff3500',
+    },
+    success: {
+      main: '#1b5e20',
+    },
+    divider: 'rgba(183,28,28,0.28)',
+    info: {
+      main: '#4db6ac',
+    },
+  },
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 600,
+  },
+  shape: {
+    borderRadius: 6,
+  },
+});

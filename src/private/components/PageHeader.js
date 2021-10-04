fd951cfa65d93e7d959b523159d9db079ea9475e
raquel <raquelanaroman@gmail.com>
2021-10-04 09:42:25 -0400

    add bottom margin to page header

--- src/private/components/PageHeader.js
+++ src/private/components/PageHeader.js
@@ -2,7 +2,7 @@ import {Box, Typography} from '@mui/material'
 
 export default function PageHeader({page, icon}) {
     return (
-        <Box sx={{ textAlign: 'left', display: 'flex', alignItems: 'center'}}>
+        <Box sx={{ textAlign: 'left', display: 'flex', alignItems: 'center', mb: 2}}>
                 {icon}
                 <Typography variant="h4" component="h1">
                     {page}

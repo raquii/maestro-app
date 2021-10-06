1e25c0973fb40c1aed255b07df84067f108f94d2
raquel <raquelanaroman@gmail.com>
2021-10-06 13:55:53 -0400

    add email selected logic, style draftjs editor

--- src/private/students/EmailDialogue.js
+++ src/private/students/EmailDialogue.js
@@ -1,25 +1,51 @@
-import { TextField } from '@mui/material';
+import { TextField, Autocomplete} from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { EditorState } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg';
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css"
-import { useState } from 'react';
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import './styles.css'
 
 
-export default function EmailDialogue({open, handleClose}) {
+export default function EmailDialogue({ settings, handleClose, selected }) {
     const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const [value, setValue] = useState([]);
+    const [showToolbar, setShowToolbar] = useState(false)
+
+    const students = useSelector(state => state.students);
+    const toOptions = students.map(s => ({ name: `${s.firstName} ${s.lastName}`, email: s.email }));
+
+    
+    useEffect(() => {
+        function setToSelected(){
+            if(settings.selectedStudents && settings.selectedParents){
+                //TODO parent emails
+            }else if(settings.selectedStudents){
+                setValue((state)=>selected.map(s=>({ name: `${s.firstName} ${s.lastName}`, email: s.email })))
+            }else if(settings.selectedParents){
+                //TODO parent emails
+            }else{
+                setValue([])
+            }
+        }
+        setToSelected()
+    }, [selected, settings])
 
     return (
         <Dialog
             maxWidth='sm'
             fullWidth
-            open={open}
-            onClose={handleClose}
+            open={settings.open}
+            onClose={(event, reason) => {
+                if (reason !== 'backdropClick') {
+                    handleClose();
+                }
+            }}
             aria-labelledby="alert-dialog-email"
             aria-describedby="alert-dialog-description"
         >
@@ -27,22 +53,72 @@ export default function EmailDialogue({open, handleClose}) {
                 New Email
             </DialogTitle>
             <DialogContent>
-                <TextField
-                 autoFocus
-                 margin="dense"
-                 id="name"
-                 label="Email Address"
-                 type="email"
-                 fullWidth
-                 variant="standard"/>
-                <Editor 
-                    editorState={editorState} 
-                    onEditorStateChange={setEditorState} 
+                <Autocomplete
+                    autoFocus
+                    size='small'
+                    sx={{mt:1}}
+                    multiple
+                    value={value}
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                    }}
+                    isOptionEqualToValue={(option, value) => option.email === value.email}
+                    id="email-to-line"
+                    options={toOptions}
+                    getOptionLabel={(option) => option.name}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            variant="outlined"
+                            label="To"
+                            placeholder="Select Recipients"
+                        />
+                    )}
                 />
+                <TextField
+                    margin="dense"
+                    id="subject"
+                    label="Subject"
+                    type="text"
+                    size='small'
+                    fullWidth
+                    variant="outlined" />
+                <Button
+                    fullWidth
+                    size='small'
+                    onClick={()=>setShowToolbar(!showToolbar)}
+                    color='secondary'
+                    variant='outlined'
+                    sx={{mb:1, mt:1}}
+                >
+                    {showToolbar ? 'Close Toolbar' : 'Open Toolbar'}
+                </Button>
+                <Editor
+                    editorState={editorState}
+                    placeholder='Body'
+                    ariaLabel='email-body'
+                    onEditorStateChange={setEditorState}
+                    toolbarClassName="draft-toolbar"
+                    wrapperClassName="draft-wrapper"
+                    editorClassName="draft-editor"
+                    toolbarHidden={showToolbar ? false : true}
+                    toolbar={{ options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'link', 'emoji'] }}
+                />
+                
             </DialogContent>
             <DialogActions>
-                <Button onClick={()=>console.log('cancel')}>Cancel</Button>
-                <Button onClick={()=>console.log('send')} autoFocus>
+                <Button
+                    onClick={handleClose}
+                    color='secondary'
+                    variant='outlined'
+                >
+                    Cancel
+                </Button>
+                <Button
+                    onClick={() => console.log('send')}
+                    autoFocus
+                    variant='contained'
+                >
                     Send
                 </Button>
             </DialogActions>

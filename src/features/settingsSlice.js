de809219d2c9adfa92796386bd107517cdf160ec
raquel <raquelanaroman@gmail.com>
2021-10-10 11:43:34 -0400

    add settings reducer logic

--- src/features/settingsSlice.js
+++ src/features/settingsSlice.js
@@ -2,34 +2,42 @@ import { createSlice } from "@reduxjs/toolkit";
 import { api } from "./api";
 
 const initialState = {
-    columnViews:{
-        firstName:false, 
-        lastName:false,
-        age:true,
-        birthday:true,
-        defaultLessonDuration:true,
-        defaultLessonPrice:true,
-        email:false,
-        phone:true,
-        family:true,
-        gender:true,
-        groups:true,
-        makeUpCredits:false,
-        status:false,
-    }
+    // columnViews: {
+    //     firstName: false,
+    //     lastName: false,
+    //     age: true,
+    //     birthday: true,
+    //     defaultLessonDuration: true,
+    //     defaultLessonPrice: true,
+    //     email: false,
+    //     phone: true,
+    //     family: true,
+    //     gender: true,
+    //     groups: true,
+    //     makeUpCredits: false,
+    //     status: false,
+    // }
 };
 
 export const slice = createSlice({
     name: "settings",
     initialState,
-    reducers:{},
+    reducers: {},
     extraReducers: (builder) => {
         builder
-            .addMatcher(api.endpoints.settings.matchFulfilled, (state, action) => {
-                console.log('fulfilled-settings', action,);
+            .addMatcher(api.endpoints.login.matchFulfilled, (state, action) => {
+                console.log('fulfilled: login - settings', action);
+                return action.payload.data.attributes.preferences.data
             })
+            .addMatcher(api.endpoints.isLoggedIn.matchFulfilled, (state, action) => {
+                console.log('fulfilled: isLoggedIn - settings', action);
+                return action.payload.data.attributes.preferences.data
+            })
+            // .addMatcher(api.endpoints.settings.matchFulfilled, (state, action) => {
+            //     console.log('fulfilled: settings', action,);
+            // })
             .addMatcher(api.endpoints.logout.matchFulfilled, (state) => {
-                console.log('fulfilled-logout', state);
+                console.log('fulfilled: logout - settings', state);
                 return initialState;
             })
 
--- src/features/store.js
+++ src/features/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { api } from './api'
 import userReducer from "./userSlice";
 import studentsReducer from './studentsSlice'
+import settingsReducer from './settingsSlice'
 
 const store = configureStore({
     reducer: {
         user: userReducer,
         students: studentsReducer,
+        settings: settingsReducer,
         [api.reducerPath]: api.reducer,
     },
     middleware: (getDefaultMiddleware) =>

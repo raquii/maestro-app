10c4baeea7ea9db0bb601c38b173baa3e38d3c71
raquel <raquelanaroman@gmail.com>
2021-10-10 17:29:24 -0400

    change header style

--- src/private/components/PageHeader.js
+++ src/private/components/PageHeader.js
@@ -1,12 +1,12 @@
-import {Box, Typography} from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
-export default function PageHeader({page, icon}) {
+export default function PageHeader({ page, icon }) {
     return (
-        <Box sx={{ textAlign: 'left', display: 'flex', alignItems: 'center', mb: 2}}>
-                {icon}
-                <Typography variant="h4" component="h1">
-                    {page}
-                </Typography>
+        <Box sx={{ textAlign: 'left', display: 'flex', alignItems: 'center', mb: 2 }}>
+            {icon}
+            <Typography variant="h3" component="h1" >
+                {page}
+            </Typography>
         </Box>
     )
 }

a51abbf72b121458ef2a839012398f7de0f493c4
raquel <raquelanaroman@gmail.com>
2021-10-15 10:21:58 -0400

    remove commented out code

--- src/features/settingsSlice.js
+++ src/features/settingsSlice.js
@@ -2,21 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 import { api } from "./api";
 
 const initialState = {
-    // columnViews: {
-    //     firstName: false,
-    //     lastName: false,
-    //     age: true,
-    //     birthday: true,
-    //     defaultLessonDuration: true,
-    //     defaultLessonPrice: true,
-    //     email: false,
-    //     phone: true,
-    //     family: true,
-    //     gender: true,
-    //     groups: true,
-    //     makeUpCredits: false,
-    //     status: false,
-    // }
 };
 
 export const slice = createSlice({

4748fbbf709117226a657d2f707a292950c3c8ee
raquel <raquelanaroman@gmail.com>
2021-10-16 16:57:21 -0400

    create student validation schema

---
+++ src/private/students/studentValidationSchema.js
@@ -0,0 +1,87 @@
+import * as yup from 'yup';
+
+const studentValidationSchema = yup.object({
+    student: yup.object({
+        firstName: yup.string()
+        .min(2, 'First name should be at least 2 characters')
+        .max(50, 'First name should be less than 50 characters')
+        .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+        .trim()
+        .required('Required'),
+        lastName: yup.string()
+            .min(2, 'First name should be at least 2 characters')
+            .max(50, 'First name should be less than 50 characters')
+            .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+            .trim()
+            .required('Required'),
+        email: yup.string()
+            .email('Enter a valid email')
+            .trim(),
+        address: yup.string(),
+        lessonReminderEmails: yup.boolean(),
+        lessonReminderSms: yup.boolean(),
+        role: yup.number().integer(),
+    }),
+    studentProfile: yup.object({
+        birthday: yup.date(),
+        adult: yup.boolean(),
+        defaultLessonPrice: yup.number()
+            .lessThan(1000, "Invalid lesson price: Please enter a value less than 1000"),
+        defaultLessonDuration: yup.number('Lesson Duration should be a number in minutes')
+            .integer()
+            .min(0, "Invalid Duration")
+            .max(120, "Invalid Lesson Length. Please select a length less than 120 minutes"),
+        status: yup.number()
+            .integer()
+            .min(0, "Invalid Status")
+            .max(4, "Invalid Status")
+            .required('Please set a status for the student'),
+        gender: yup.string(),
+        grade: yup.string().max(10, "Should be less than 10 characters."),
+        school: yup.string()
+    }),
+    guardian1: yup.object({
+        firstName: yup.string()
+        .min(2, 'First name should be at least 2 characters')
+        .max(50, 'First name should be less than 50 characters')
+        .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+        .trim()
+        .required('Required'),
+        lastName: yup.string()
+            .min(2, 'First name should be at least 2 characters')
+            .max(50, 'First name should be less than 50 characters')
+            .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+            .trim()
+            .required('Required'),
+        email: yup.string()
+            .email('Enter a valid email')
+            .trim(),
+        address: yup.string(),
+        lessonReminderEmails: yup.boolean(),
+        lessonReminderSms: yup.boolean(),
+        role: yup.number().integer(),
+    }),
+    guardian2: yup.object({
+        firstName: yup.string()
+        .min(2, 'First name should be at least 2 characters')
+        .max(50, 'First name should be less than 50 characters')
+        .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+        .trim()
+        .required('Required'),
+        lastName: yup.string()
+            .min(2, 'First name should be at least 2 characters')
+            .max(50, 'First name should be less than 50 characters')
+            .matches(/^([A-ZÀ-ÿa-z][-,À-ÿa-z. ']+[ ]*)+$/, "Invalid name characters")
+            .trim()
+            .required('Required'),
+        email: yup.string()
+            .email('Enter a valid email')
+            .trim(),
+        address: yup.string(),
+        lessonReminderEmails: yup.boolean(),
+        lessonReminderSms: yup.boolean(),
+        role: yup.number().integer(),
+    }),
+});
+
+export default studentValidationSchema;

b1513e30bea97176237367809f188bb36d195307
raquel <raquelanaroman@gmail.com>
2021-10-19 19:41:45 -0400

    add recurringevent create logic

--- src/features/eventsSlice.js
+++ src/features/eventsSlice.js
@@ -26,8 +26,33 @@ export const slice = createSlice({
                 const newEvent = {id: action.payload.data.id, ...action.payload.data.attributes}
                 return [...state, newEvent]
             })
+            .addMatcher(api.endpoints.createRecurringEvent.matchFulfilled, (state, action) => {
+                console.log('fulfilled: createRecurringEvent', action);
+                const newEvents = [...state, ...action.payload.data.map(e=>({id: e.id, ...e.attributes}))]
+                return newEvents
+            })
             .addMatcher(api.endpoints.updateEvent.matchFulfilled, (state, action) => {
                 console.log('fulfilled: updateEvent', action,);
+                // return action.payload.data
+            })
+            .addMatcher(api.endpoints.updateFutureEventOccurances.matchFulfilled, (state, action) => {
+                console.log('fulfilled: updateFutureEvents', action,);
+                // return action.payload.data
+            })
+            .addMatcher(api.endpoints.updateAllEventOccurances.matchFulfilled, (state, action) => {
+                console.log('fulfilled: updateAllEvents', action,);
+                // return action.payload.data
+            })
+            .addMatcher(api.endpoints.deleteEvent.matchFulfilled, (state, action) => {
+                console.log('fulfilled: deleteEvent', action,);
+                return state.filter(event => event.id !== action.payload.id)
+            })
+            .addMatcher(api.endpoints.deleteFutureEventOccurances.matchFulfilled, (state, action) => {
+                console.log('fulfilled: deleteFutureEvents', action,);
+                // return action.payload.data
+            })
+            .addMatcher(api.endpoints.deleteAllEventOccurances.matchFulfilled, (state, action) => {
+                console.log('fulfilled: deleteAllEvents', action,);
                 // return action.payload.data
             })
             .addMatcher(api.endpoints.logout.matchFulfilled, (state) => {

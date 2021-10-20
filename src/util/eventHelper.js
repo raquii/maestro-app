1e65d8e6b27d317b0e4966a37e531f1e1ebf0ee6
raquel <raquelanaroman@gmail.com>
2021-10-19 21:03:56 -0400

    clean up logic for recurring/existing events

--- src/util/eventHelper.js
+++ src/util/eventHelper.js
@@ -1,10 +1,17 @@
 import * as dayjs from 'dayjs';
 
-export default function eventHelper(data){
-    console.log(data)
-    const allKeys = {
-        teacherId: data.teacherId,
-        studentId: data.student.id,
+//find the end datetime using duration
+function calculateEnd(date, time, duration) {
+    return dayjs(date + time).add(duration, 'm').format();
+};
+
+export default function eventHelper(data) {
+    console.log('in event helper:', data)
+    
+    const end = calculateEnd(data.date, data.time, data.duration);
+
+    const event = {
+        studentProfileId: data.studentProfile === null ? null : data.studentProfile.id,
         allDay: data.allDay,
         title: data.title,
         allowRegistration: data.allowRegistration,
@@ -14,18 +21,27 @@ export default function eventHelper(data){
         location: data.location,
         price: data.price,
         defaultLesson: data.defaultLesson,
+        start: dayjs(data.date + data.time).format(),
+        end: end,
+        id: data.id === undefined ? undefined : data.id,
+    };
+ 
+    //if the event WAS recurring
+    if (data.recurringGroupId) {
+        //if the event was but is no longer recurring, set groupId to null
+        if (!data.recurring) {
+            return { ...event, recurringGroupId: null }
+        //otherwise, include it in the object
+        } else {
+            return { event: { ...event, recurringGroupId: data.recurringGroupId }, recurringGroup: { endDate: data.endDate } }
+        }
+    }
+
+    //if the event is newly a recurring/defaultLesson
+    if (data.recurring || data.defaultLesson) {
+        return { event: { ...event }, recurringGroup: { endDate: data.endDate } }
     };
 
-
-    if(data.recurring || data.defaultLesson){
-        let endTime = calculateDuration(data.date, data.time, data.duration);
-        return {...allKeys, startTime: data.time, endTime: endTime, startRecur:data.date, endRecur: data.endRecur, daysOfWeek: dayjs(data.date).format('d') };
-    }else{
-        let end = calculateDuration(data.date, data.time, data.duration);
-        return {...allKeys, start: dayjs(`${data.date}T${data.time}`).toISOString(), end:end};
-    };
-};
-
-function calculateDuration(date, time, duration){
-    return dayjs(date + time).add(duration, 'm').format('HH:mm');
+    //if the event doesn't recur and never has
+    return event
 };

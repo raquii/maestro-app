9d01a6c9ff253beed13294c08cac207f0b7e73a5
raquel <raquelanaroman@gmail.com>
2021-10-26 12:41:06 -0400

    remove useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux'
 
-import {useIsLoggedInMutation} from './features/api'
+import { useIsLoggedInMutation } from './features/api'
 import Private from './private/Private';
 import Public from './public/Public';
 import './App.css';
 
 
 function App() {
-  const [isLoggedIn] = useIsLoggedInMutation();
+  const [isLoggedIn, { isSuccess }] = useIsLoggedInMutation();
   const history = useHistory();
   const email = useSelector(state => state.user.email);
 
-  const checkForToken = useCallback(async () => {
-    try {
-      await isLoggedIn().unwrap()
-      history.push('/dashboard')
-    } catch (error) {
-      console.error()
-      history.push('/welcome')
-    }
-  }, [isLoggedIn, history]);
 
   useEffect(() => {
-    checkForToken()
-  }, [checkForToken]);
+    isLoggedIn()
+      .unwrap()
+      .then(() => {
+          history.push('/dashboard')
+      })
+    .catch(()=>history.push('/welcome'))
+  }, [history, isLoggedIn]);
 
   return (
     <div className="App">
-      {email.length > 0 ? 
-      <Private/> 
-      :
-      <Public/>
+      {email.length > 0 ?
+        <Private />
+        :
+        <Public />
       }
     </div>
   );

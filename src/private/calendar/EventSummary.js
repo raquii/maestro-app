bb30b589e00cc15a531054f946ac9550f9d23850
raquel <raquelanaroman@gmail.com>
2021-10-26 12:45:16 -0400

    change styling for event summary

--- src/private/calendar/EventSummary.js
+++ src/private/calendar/EventSummary.js
@@ -1,15 +1,16 @@
 import { Dialog, DialogTitle, DialogContent, DialogActions, Grid, Button, Divider, Typography } from "@mui/material";
+import dayjs from "dayjs";
 import { useHistory } from 'react-router';
 
 export default function EventSummary({ url, event, closeMenu, open, setSelectedEvent }) {
     const history = useHistory();
 
-    function handleEdit(id){
+    function handleEdit(id) {
         history.push(`${url}/event-details/${id}`);
         closeMenu();
     }
 
-    function handleClose(){
+    function handleClose() {
         setSelectedEvent({});
         closeMenu();
     }
@@ -17,25 +18,17 @@ export default function EventSummary({ url, event, closeMenu, open, setSelectedE
     return (
         <Dialog open={open}>
             <DialogTitle id="alert-confirm-send-login-emails">
-                Event Summary
+                {event.title}
             </DialogTitle>
             <DialogContent>
-                <Grid container spacing={1}>
-
+                <Grid container>
                     <Grid item xs={12}>
-                        {event.title}
+                        <Divider />
                     </Grid>
                     <Grid item xs={12}>
-                        
-                    </Grid>
-                    <Grid item xs={12}>
-                        
-                    </Grid>
-                    <Grid item xs={12}>
-
-                    </Grid>
-                    <Grid item xs={12}>
-
+                        <Typography variant="overline" color="initial" fontSize="medium">
+                            {dayjs(event.start).format('MMMM D, YYYY')}  • {event.allDay ? "All Day" : `${dayjs(event.start).format('h:mm a')} - ${dayjs(event.end).format('h:mm a')}`}
+                        </Typography>
                     </Grid>
 
                 </Grid>
@@ -50,7 +43,7 @@ export default function EventSummary({ url, event, closeMenu, open, setSelectedE
                     Cancel
                 </Button>
                 <Button
-                    onClick={()=>handleEdit(event.id)}
+                    onClick={() => handleEdit(event.id)}
                     variant='contained'
                     color="primary"
                 >
